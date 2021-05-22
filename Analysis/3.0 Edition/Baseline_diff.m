a5c4e96e61f7410cf52a25d095192faa98c120cf
YuangCheng <yuang_cheng@pku.edu.cn>
2021-05-23 03:16:26 +0800

    Update Baseline_diff.m

--- Analysis/3.0 Edition/Baseline_diff.m
+++ Analysis/3.0 Edition/Baseline_diff.m
@@ -1,7 +1,4 @@
-%% based on jackknife
 clear
-idx_odd = [1,2,5,8,11,13,16,19,21,23,25]; % odd
-idx_even = [3,4,6,7,9,10,12,14,15,17,18,20,22,24]; % even
 load PSD.mat
 fs = 30;
 N = 64;
@@ -16,24 +13,30 @@ figure(3)
 subplot(1,2,1);
 PSD_even_diff = PSD_post_even - PSD_pretest_even;
 shadedErrorBar(f',mean(PSD_even_diff,2),nanstd(PSD_even_diff,[],2)/sqrt(size(PSD_even_diff,2)));
+
 n_perms = 10000;
 [datobs, datrnd] = cluster_test_helper(PSD_even_diff,n_perms); % randomize data at each time-point
-[h, p, clusterinfo] = cluster_test(datobs,datrnd,1,0.05,0.05,'sum');
+[h, p, clusterinfo] = cluster_test(datobs,datrnd,1,0.05,0.01,'sum');
 fprintf('Out of %d tests, %d is significant. \n',length(h),sum(h));
-h(h==0)=NaN;
-
+sig = NaN(size(h));
+sig(h) = max(mean(PSD_even_diff,2)).*1.5;
+hold on;
+plot(f,sig,'r-','LineWidth',2.5);
 xlabel('Frequency (Hz)');
-ylabel('PSD (a.u.)')
+ylabel('PSD (post - pre) (a.u.)')
 title('5Hz prime group')
 subplot(1,2,2);
 PSD_odd_diff = PSD_post_odd - PSD_pretest_odd;
 shadedErrorBar(f',mean(PSD_odd_diff,2),nanstd(PSD_odd_diff,[],2)/sqrt(size(PSD_odd_diff,2)));
+
 n_perms = 10000;
 [datobs, datrnd] = cluster_test_helper(PSD_even_diff,n_perms); % randomize data at each time-point
-[h, p, clusterinfo] = cluster_test(datobs,datrnd,1,0.05,0.05,'sum');
+[h, p, clusterinfo] = cluster_test(datobs,datrnd,1,0.05,0.01,'sum');
 fprintf('Out of %d tests, %d is significant. \n',length(h),sum(h));
-h(h==0)=NaN;
-
+sig = NaN(size(h));
+sig(h) = max(mean(PSD_even_diff,2)).*1.5;
+hold on;
+plot(f,sig,'r-','LineWidth',2.5);
 xlabel('Frequency (Hz)');
-ylabel('PSD (a.u.)')
+ylabel('PSD (post - pre)(a.u.)')
 title('3Hz prime group')

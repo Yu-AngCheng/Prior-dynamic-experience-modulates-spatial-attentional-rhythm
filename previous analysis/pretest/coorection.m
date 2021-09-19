ac5c446fd9d8b9f4df782cdc5fdb052ffc2acc46
YuangCheng <yuang_cheng@pku.edu.cn>
2021-09-19 18:46:59 +0800

    update

--- previous analysis/pretest/coorection.m
+++
@@ -1,9 +0,0 @@
-originalsignal=tempsignal;
-for k=1:size(originalsignal,3)
-    idx=originalsignal(:,4,k)==0;
-    idxx=isnan(originalsignal(:,4,k));
-    originalsignal(idx,4,k)=2;
-    originalsignal(:,4,k)=double(originalsignal(:,4,k)==originalsignal(:,2,k));
-    originalsignal(idxx,4,k)=NaN;
-end
-tempsignal_correction=originalsignal;
